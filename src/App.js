3b5b4a782bade64135011fb93933e1aedcec309e
tangweikun <819573105@qq.com>
2020-03-05 14:36:51 +0800

    d

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>888</div>
+      <input type="file" accept="image/*" capture="camera" />
+    </div>
+  );
+}
+
+export default App;
